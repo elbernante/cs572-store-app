68c5ec1bd8a8f1b07ce33ee9fb5a7939c7364328
Peter James Bernante <elbernante@gmail.com>
2017-12-07 22:54:41 -0600

    feat: Add Product detail

--- src/app/product-detail.component.ts
+++ src/app/product-detail.component.ts
@@ -6,6 +6,22 @@ import { Product } from './product';
   template: `
     <div *ngIf="product">
       <h2> {{ product.name }} Details</h2>
+      <div><span>ID:</span> {{ product.id }}</div>
+      <div>
+        <label>Name:
+          <input [(ngModel)]="product.name" placeholder="Name" >
+        </label>
+      </div>
+      <div>
+        <label>Price:
+          <input type="number" [(ngModel)]="product.price" placeholder="Price" >
+        </label>
+      </div>
+      <div>
+        <label>Description:
+          <input [(ngModel)]="product.description" placeholder="Description" >
+        </label>
+      </div>
       <button (click)="requestDelete()">Delete</button>
     </div>
   `,
